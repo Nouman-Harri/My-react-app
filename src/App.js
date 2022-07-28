cf86f1c9b667d4cfcf0e15335eaded1944706200
Nouman-Harri <noumanharry7@gmail.com>
2022-07-28 23:28:28 +0500

    router add

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import Alert from "./components/Alert";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
@@ -14,33 +15,54 @@ function App() {
       msg: message,
       type: type,
     });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
   };
 
   const toggleMode = () => {
     if (mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "#183969";
-      showAlert("dark mode has been enabled", "sucess");
+      showAlert("Dark mode has been enabled", "sucess");
+      document.title = "Harry - Dark mode";
+      // setInterval(() => {
+      //   document.title = "install my app";
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title = "subscribe my app";
+      // }, 1000);
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
-      showAlert("light mode has been enabled", "sucess");
+      showAlert("Light mode has been enabled", "sucess");
+      document.title = "Harry - Light mode";
     }
   };
   return (
     <>
-      <Navbar
-        title="Harry"
-        aboutText="About"
-        mode={mode}
-        toggleMode={toggleMode}
-      />
-      <Alert Alert={alert} />
-      {/* <Navbar /> */}
-      <div className="container my-3">
-        <About />
-        <TextForm heading="Enter the text to anaylize below" mode={mode} />
-      </div>
+      <Router>
+        <Navbar
+          title="Harry"
+          aboutText="About"
+          mode={mode}
+          toggleMode={toggleMode}
+        />
+        <Alert alert={alert} />
+        <div className="container my-3">{/* <About /> */}</div>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <TextForm
+              showAlert={showAlert}
+              heading="Enter the text to anaylize below"
+              mode={mode}
+            />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,38 +5,45 @@ export default function TextForm(props) {
   const handlerUpClick = () => {
     let newText = text.toUpperCase();
     setText(newText);
+    props.showAlert("convert to uppercase", "sucess");
   };
   // this function is for to convert the data into Lowercase
   const handlerLoClick = () => {
     let newText = text.toLowerCase();
     setText(newText);
+    props.showAlert("convert to lowercase", "sucess");
   };
   // this function is for to clear the text
   const handlerClearClick = () => {
-    let newText =''
+    let newText = "";
     setText(newText);
+    props.showAlert("texted cleared", "sucess");
   };
   const handelOnchange = (event) => {
     setText(event.target.value);
   };
   // this function is for the copy the text
-  const handleCopy =() => {
+  const handleCopy = () => {
     var text = document.getElementById("myBox");
     text.select();
-    navigator.clipboard.writeText(text.value)
-  }
+    navigator.clipboard.writeText(text.value);
+    props.showAlert("texted copied", "sucess");
+  };
   //  this function is to remove the extra spacres that have been added into the text
   const handelExtraSpace = () => {
     let newText = text.split(/[ ]+/);
     setText(newText.join(" "));
-  }
-  
-  
+    props.showAlert("Extra spaces is removed", "sucess");
+  };
+
   const [text, setText] = useState("");
   // setText("start here");
   return (
     <>
-      <div className="container my-3" style={{color : props.mode === 'dark' ? 'white': '#183969'}}>
+      <div
+        className="container my-3"
+        style={{ color: props.mode === "dark" ? "white" : "#183969" }}
+      >
         <h1>{props.heading}</h1>
         <div className="mb-3">
           <label htmlFor="myBox" className="form-label">
@@ -45,7 +52,10 @@ export default function TextForm(props) {
           <textarea
             className="form-control"
             value={text}
-            style={{backgroundColor : props.mode === 'dark' ? 'grey': 'white', color: props.mode === 'dark' ? 'white': '#183969'}}
+            style={{
+              backgroundColor: props.mode === "dark" ? "grey" : "white",
+              color: props.mode === "dark" ? "white" : "#183969",
+            }}
             onChange={handelOnchange}
             id="myBox"
             rows="6"
@@ -67,14 +77,21 @@ export default function TextForm(props) {
           Remove extra spaces
         </button>
       </div>
-        <div className="container"style={{color : props.mode === 'dark' ? 'white': '#183969'}} >
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "#183969" }}
+      >
         <h2>your text summary</h2>
         <p>
           {text.split(" ").length} words and {text.length} characters
         </p>
         <p>{0.008 * text.split(" ").length} minutes to read</p>
         <h2>Preview</h2>
-        <p>{text.length>0?text:"Enter something in the box to preview it here"}</p>
+        <p>
+          {text.length > 0
+            ? text
+            : "Enter something in the box to preview it here"}
+        </p>
       </div>
     </>
   );
